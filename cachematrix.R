91d81fffb3cbfd1c8ea8dcffa199b0ef81d28856
Jeroenzai <jeroenvb@protonmail.com>
2017-07-26 18:57:37 +0200

    programming assignment 2 Cousera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCache creates a stored value of your desired value to calculate, 
+## if already found by cacheSolve, it will use the already cached value
+## if not it will calculate and store the value
 
-## Write a short comment describing this function
+## This function has a get and set for x and for i
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setmean <- function(inverse) i <<- inverse
+  getmean <- function() i
+  list(set=set, get=get,
+       getmean=getmean, setmean=setmean)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getmean()
+    if(!is.null(i)) {
+      message("getting cached data")
+      return(i)
+    }
+  data <- x$get()
+  i <- solve(data)
+  x$setmean(i)
+  i 
+  ## Return a matrix that is the inverse of 'x'
 }
